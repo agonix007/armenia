f2554f2abbbe1e622e448f79cb20e1c2878a3adf
Bikram <technology2203@gmail.com>
2023-06-29 23:15:04 +0530

    demo of products data

---
+++ data/products.js
@@ -0,0 +1,9 @@
+{
+    pname: "Ladies Belt",
+    description: "Elevate your style with our elegant and versatile ladies belt, perfect for any outfit or occasion.",
+    price: 280.00,
+    category: "Females",
+    brand: "Sree Leathers",
+    quantity: 100,
+    imageUrl: "https://i.ibb.co/kJxcsjf/belt.png"
+}
