056e6d5063a7eda9e4f59cc4aee143874418b95b
Apurba Dutta <apurbadutta2099@gmail.com>
2023-06-30 00:18:08 +0530

    data add coma

--- data/products.js
+++ data/products.js
@@ -6,4 +6,13 @@
     brand: "Sree Leathers",
     quantity: 100,
     imageUrl: "https://i.ibb.co/kJxcsjf/belt.png"
+},
+{
+    pname: "Nike Yellow shoe",
+    description: "Nike shoes Unleash your potential with stylish comfort and unmatched performance.",
+    price: 1300.00,
+    category: "Male",
+    brand: "Nike",
+    quantity: 80,
+    imageUrl: "https://i.ibb.co/GFWCwk0/f-img-3-1.png"
 }
