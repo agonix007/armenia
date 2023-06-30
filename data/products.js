a60bb6929df44088ae50497ff1d164adfbbd92c5
Apurba Dutta <apurbadutta2099@gmail.com>
2023-06-30 21:42:07 +0530

    add more products

--- data/products.js
+++ data/products.js
@@ -1,13 +1,4 @@
 {
-    pname: "Ladies Belt",
-    description: "Elevate your style with our elegant and versatile ladies belt, perfect for any outfit or occasion.",
-    price: 280.00,
-    category: "Females",
-    brand: "Sree Leathers",
-    quantity: 100,
-    imageUrl: "https://i.ibb.co/kJxcsjf/belt.png"
-},
-{
     pname: "Nike Yellow shoe",
     description: "Nike shoes Unleash your potential with stylish comfort and unmatched performance.",
     price: 1300.00,
@@ -15,4 +6,121 @@
     brand: "Nike",
     quantity: 80,
     imageUrl: "https://i.ibb.co/GFWCwk0/f-img-3-1.png"
+},
+{
+    pname: "Ladies Belt",
+    description: "Elevate your style with our elegant and versatile ladies belt, perfect for any outfit or occasion.",
+    price: 280.00,
+    category: "Females",
+    brand: "Sree Leathers",
+    quantity: 100,
+    imageUrl: "https://i.ibb.co/0BS4TMp/ladies-Belt.jpg"
+},
+{
+    pname: "Unisex Nike Bag",
+    description: "Effortlessly stylish and versatile, our unisex bag is the ultimate accessory for on-the-go individuals seeking",
+    price: 800.00,
+    category: "Unisex",
+    brand: "Nike",
+    quantity: 125,
+    imageUrl: "https://i.ibb.co/FqBY5PQ/bag.png"
+},
+{
+    pname: "Ladies Bag",
+    description: "Exude timeless elegance with our female bag, a perfect blend of style and functionality or occasion.",
+    price: 1499.00,
+    category: "Females",
+    brand: "Sree Leathers",
+    quantity: 88,
+    imageUrl: "https://i.ibb.co/VLWChhF/ibag.webp"
+},
+{
+    pname: "Male Belt",
+    description: "Elevate your style with our elegant and versatile gents belt, perfect for any outfit or occasion",
+    price: 180.00,
+    category: "Males",
+    brand: "Leathers Hub",
+    quantity: 499,
+    imageUrl: "https://i.ibb.co/4WPCbmc/malebelt.jpg"
+},
+{
+    pname: "Female Money Bag",
+    description: "Stylish and spacious money bag designed exclusively for the modern woman on the go.",
+    price: 400,
+    category: "Females",
+    brand: "Leathers Hub",
+    quantity: 223,
+    imageUrl: "https://i.ibb.co/2yzhnzD/Female-Money-Bag.jpg"
+},
+{
+    pname: "Male Money Bag",
+    description: "Sleek and functional money bag designed for the discerning gentleman who values both style and practicality.",
+    price: 310,
+    category: "Males",
+    brand: "Leathers Hub",
+    quantity: 223,
+    imageUrl: "https://i.ibb.co/9HZ1BMs/Male-Moneybag.png"
+},
+{
+    pname: "Red Nike",
+    description: "Comfortable and versatile shoe that effortlessly combines fashion and functionality for all-day wear.",
+    price: 220,
+    category: "Males",
+    brand: "Leathers Hub",
+    quantity: 333,
+    imageUrl: "https://i.ibb.co/3B2wcHf/RedNike.png"
+},
+{
+    pname: "Side Bag",
+    description: "Trendy and convenient side bag designed specifically for men, providing ample storage space for everyday essentials.",
+    price: 299,
+    category: "Males",
+    brand: "Leathers Hub",
+    quantity: 1100,
+    imageUrl: "https://i.ibb.co/t8qGtLC/side.png"
+},
+{
+    pname: "Hide Skin Belt",
+    description: "  A premium belt made from genuine hide skin, adding a touch of sophistication to any outfit.",
+    price: 990,
+    category: "Males",
+    brand: "Leather hub",
+    quantity: 283,
+    imageUrl: "https://i.ibb.co/HpJ2HjV/Hide-Skin-Belt.jpg"
+},
+{
+    pname: "Men office shoe",
+    description: " Elegant and professional leather shoe designed to keep you comfortable and stylish throughout the workday.",
+    price: 1200,
+    category: "Males",
+    brand: "OMNI LEATHERS",
+    quantity: 554,
+    imageUrl: "https://i.ibb.co/c6vsZmz/Office-Shoe.jpg"
+},
+{
+    pname: "Uni-Sex Sling Bag",
+    description: " Versatile and functional sling bag suitable for both men and women, offering convenient storage and a modern design.",
+    price: 8990,
+    category: "Males",
+    brand: "OMNI LEATHERS",
+    quantity: 880,
+    imageUrl: "https://i.ibb.co/ZRQ07n0/Unisex-Sling-Bag.jpg"
+},
+{
+    pname: "Woman Traditionals",
+    description: "  Exquisite traditional shoe for women, showcasing cultural heritage with a blend of timeless elegance.",
+    price: 1990,
+    category: "Females",
+    brand: "TrendiBazar",
+    quantity: 283,
+    imageUrl: "https://i.ibb.co/vmxD8kC/Woman-Sandel.jpg"
+},
+{
+    pname: "Woman leather Sandel",
+    description: "Stylish and comfortable leather sandal designed exclusively for women, perfect for casual outings and summer adventures.",
+    price: 690,
+    category: "Females",
+    brand: "TrendiBazar",
+    quantity: 753,
+    imageUrl: "https://i.ibb.co/YdJnd9r/woman-Traditional.jpg"
 }
