0be49a765e18cf158cee6cee4ee7b10d49e54006
Bikram <technology2203@gmail.com>
2023-07-26 13:27:18 +0530

    Fixed outings and cart page

---
+++ src/routers/frontend.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = new express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.get("/products", (req, res) => {
+  res.render("allProducts");
+});
+router.get("/cart", (req, res) => {
+  res.render("cart");
+});
+router.get("/checkout", (req, res) => {
+  res.render("checkout");
+});
+router.get("/about", (req, res) => {
+  res.render("aboutUs");
+});
+router.get("/successful", (req, res) => {
+  res.render("successful");
+});
+router.get("*", (req, res) => {
+  res.status(404).send(`<h1>404 Page not found</h1>`);
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const routes = require("./routers/index");
+const frontend_routes = require("./routers/frontend");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -36,33 +37,35 @@ app.set("views", templatePath);
 
 hbs.registerPartials(partialPath);
 
-app.get("/", (req,res)=>{
-  res.render("index");
-})
-app.get("/login", (req,res)=>{
-  res.render("login")
-})
-app.get("/register", (req,res)=>{
-  res.render("register")
-})
-app.get("/products", (req,res)=>{
-  res.render("allProducts")
-})
-app.get("/cart", (req,res)=>{
-  res.render("cart")
-})
-app.get("/checkout", (req,res)=>{
-  res.render("checkout")
-})
-app.get("/about", (req,res)=>{
-  res.render("aboutUs")
-})
-app.get("/successful", (req,res)=>{
-  res.render("successful")
-})
-app.get("*", (req, res) => {
-  res.status(404).send(`<h1>404 Page not found</h1>`);
-});
+app.use("/", frontend_routes);
+
+// app.get("/", (req,res)=>{
+//   res.render("index");
+// })
+// app.get("/login", (req,res)=>{
+//   res.render("login")
+// })
+// app.get("/register", (req,res)=>{
+//   res.render("register")
+// })
+// app.get("/products", (req,res)=>{
+//   res.render("allProducts")
+// })
+// app.get("/cart", (req,res)=>{
+//   res.render("cart")
+// })
+// app.get("/checkout", (req,res)=>{
+//   res.render("checkout")
+// })
+// app.get("/about", (req,res)=>{
+//   res.render("aboutUs")
+// })
+// app.get("/successful", (req,res)=>{
+//   res.render("successful")
+// })
+// app.get("*", (req, res) => {
+//   res.status(404).send(`<h1>404 Page not found</h1>`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
