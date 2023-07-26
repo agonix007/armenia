f3186cbec44147c22319c2450f8f29394f0a4bd5
Bikram <technology2203@gmail.com>
2023-07-26 14:08:46 +0530

    fixed each product

--- public/modules/eachProduct.js
+++ public/modules/eachProduct.js
@@ -1,5 +1,12 @@
 import config from "../config/config.js";
 
+toastr.options = {
+  positionClass: "toast-bottom-right",
+  closeButton: true,
+  progressBar: true,
+  timeOut: 3000,
+};
+
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get("id");
 
@@ -64,3 +71,53 @@ const getProductDetails = async (productId) => {
 };
 
 getProductDetails(productId);
+
+const account = document.getElementById("account");
+const cart = document.getElementById("cartFeature");
+const auth = document.getElementById("authentication");
+
+const isLoggedIn = localStorage.getItem("username") !== null;
+const userSection = document.getElementById("username");
+
+if (isLoggedIn) {
+  let username = localStorage.getItem("username");
+  let firstname = username.split(" ")[0];
+
+  userSection.innerText = `${firstname}`;
+  account.style.display = "block";
+  cart.style.display = "block";
+} else {
+  auth.classList.remove("authenticate");
+  account.style.display = "none";
+  cart.style.display = "none";
+}
+
+const logoutUser = async () => {
+  try {
+    const response = await fetch(config.url + "/auth/logout", {
+      method: "GET",
+      headers: {
+        Authorization: localStorage.getItem("token"), // Replace with the stored token
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      toastr.success("The Adventure Awaits");
+      // Reloads the page
+      setTimeout(() => {
+        // location.reload();
+        window.location.href = "/";
+      }, 1500);
+    } else {
+      throw new Error("Internal server error");
+    }
+  } catch (error) {
+    console.log(error.message);
+    toastr.error(error.message);
+  }
+};
+
+const logout = document.getElementById("logout");
+logout.addEventListener("click", logoutUser);
