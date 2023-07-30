773edb5b91086ba4c014577ef5a9da87509a5434
Bikram <technology2203@gmail.com>
2023-07-31 04:17:20 +0530

    Client side product added

--- public/modules/admin.js
+++ public/modules/admin.js
@@ -7,6 +7,53 @@ toastr.options = {
   timeOut: 3000,
 };
 
+// Adding products in DataBase
+const addingProducts = async (event) => {
+  event.preventDefault();
+
+  const pname = document.getElementById("pname").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+  const sprice = document.getElementById("sprice").value;
+  const categories = document.getElementById("categories").value;
+  const brand = document.getElementById("brand").value;
+  const quantity = document.getElementById("quantity").value;
+  const url = document.getElementById("url").value;
+
+  const productData = {
+    pname: pname,
+    description: description,
+    price: price,
+    sprice: sprice,
+    category: categories,
+    brand: brand,
+    quantity: quantity,
+    imageUrl: url,
+  };
+
+  try {
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    toastr.success("Product Added Successfully");
+    if (!response.ok) {
+      const errorMsg = await response.json();
+      toastr.warning(errorMsg);
+      return;
+    }
+  } catch (error) {
+    console.error(error.message);
+    toastr.error(error.message);
+  }
+};
+
+const addProducts = document.getElementById("addProducts");
+addProducts.addEventListener("submit", addingProducts);
+
 const account = document.getElementById("account");
 const cart = document.getElementById("cartFeature");
 const auth = document.getElementById("authentication");
@@ -32,7 +79,7 @@ const adminPanel = async () => {
   const adminPanelLink = document.getElementById("adminPanelLink");
   const response = await fetch("/api/account");
   const data = await response.json();
-  if(data.bio === "Admin"){
+  if (data.bio === "Admin") {
     adminPanelLink.style.display = "block";
   }
 };
